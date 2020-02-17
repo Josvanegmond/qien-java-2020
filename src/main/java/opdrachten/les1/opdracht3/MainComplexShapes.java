ce2b984e67d8c28e73b4dc0bc10ebffc52844a2a
Joozey <jostievanegmond@gmail.com>
2020-02-17 20:33:05 +0100

    small error correction in description of assignment 3

--- src/main/java/opdrachten/les1/opdracht3/MainComplexShapes.java
+++ src/main/java/opdrachten/les1/opdracht3/MainComplexShapes.java
@@ -8,9 +8,9 @@ public class MainComplexShapes {
 
     // TODO: create a ComplexShapeCalculator class
     //  - Add methods that calculate the area and edge size of complex shapes
-    //  - The methods have a size argument and a boolean to determine if they return
-    //  - Use the utility classes directly from package "opdracht2"
-    //  - Expand them with more primitive shapes as you see fit
+    //  - The methods have a size argument
+    //  - Use the ShapeAreaCalculator class from package "opdracht2"
+    //  --
     //  - Shapes:
     //  - A heart made from a diamond shape and two half-circles
     //  - A snowman made of three stacked circles, each a quarter smaller than the previous one
@@ -18,15 +18,15 @@ public class MainComplexShapes {
     public static void main(String[] args) {
 
 //         TODO: uncomment if the methods are created
-//        double heartArea = getHeartArea(5);
-//        double heartEdge = getHeartEdge(5);
+//        double heartArea = ComplexShapeCalculator.getHeartArea(5);
+//        double heartEdge = ComplexShapeCalculator.getHeartEdge(5);
 
 //         FIXME: ? should be replaced by proper values.
         System.out.println("Heart of size ? has an area of ? and an edge length of ?.");
 
 //         TODO: uncomment if the methods are created
-//        double snowmanArea = getSnowmanArea(8);
-//        double snowmanEdge = getSnowmanEdge(8);
+//        double snowmanArea = ComplexShapeCalculator.getSnowmanArea(8);
+//        double snowmanEdge = ComplexShapeCalculator.getSnowmanEdge(8);
 
 //         FIXME: ? should be replaced by proper values.
         System.out.println("Snowman of base size ? has an area of ? and an edge length of ?.");
